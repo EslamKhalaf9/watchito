f6fa11c8f3a152f59ceb64b7178ac12b24e5e48a
eslamkhalaf9 <eslamkhalaf191@gmail.com>
2020-04-18 16:28:05 +0200

    Updates

---
+++ precache-manifest.ee0299e383f781f6ec8b893ebe1d1975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7169a67f69ae7c58238d00e7de85fd",
+    "url": "/watchito/index.html"
+  },
+  {
+    "revision": "b66859d3c6df867d20ac",
+    "url": "/watchito/static/css/main.45b3f7c9.chunk.css"
+  },
+  {
+    "revision": "d20050770d010fed0122",
+    "url": "/watchito/static/js/2.731d3c7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watchito/static/js/2.731d3c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66859d3c6df867d20ac",
+    "url": "/watchito/static/js/main.7b9f5761.chunk.js"
+  },
+  {
+    "revision": "ca1ad07cfc4269cdd0a2",
+    "url": "/watchito/static/js/runtime-main.b2d05708.js"
+  }
+]);
